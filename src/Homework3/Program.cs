47a57c8c3c8150e5edfe7e5581b74d7738ddcbb6
PavelAndaliukevich <apv91@yandex.by>
2021-01-10 16:01:11 +0300

    refactor: replace code to best read

--- src/Homework3/Program.cs
+++ src/Homework3/Program.cs
@@ -8,18 +8,18 @@ namespace DateHandler
         {
             while (true)
             {
-                Console.WriteLine("Введите дату чтобы узнать день недели!");
+                Console.Write("Введите дату чтобы узнать день недели: ");
                 try
 
                 {
                     var Date = Convert.ToDateTime(Console.ReadLine());
                     Console.WriteLine("День недели: " + Date.DayOfWeek);
-                    Console.WriteLine("_________________________________________");
+                    Console.WriteLine("********************");
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("Введите корректную дату в формате: ДД/ММ/ГГГГ или ДД.ММ.ГГГГ");
-                    Console.WriteLine("_________________________________________");
+                    Console.WriteLine("********************");
                 }
 
             }
