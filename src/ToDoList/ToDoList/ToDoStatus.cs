c06b8bd6f3a6ad4a9b6914ce7c7d0870259dccf8
PavelAndaliukevich <apv91@yandex.by>
2021-01-10 17:22:02 +0300

    init: add hw

---
+++ src/ToDoList/ToDoList/ToDoStatus.cs
@@ -0,0 +1,34 @@
+﻿namespace TeachMeSkills.DotNet.TodoApp
+{
+    /// <summary>
+    /// TodoStatus.
+    /// </summary>
+    public enum TodoStatus
+    {
+        /// <summary>
+        /// Unknown.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Backlog.
+        /// </summary>
+        Backlog = 1,
+
+        /// <summary>
+        /// In progress.
+        /// </summary>
+        InProgress = 2,
+
+        /// <summary>
+        /// Done.
+        /// </summary>
+        Done = 3,
+
+        /// <summary>
+        /// Canceled.
+        /// Canceled.
+        /// </summary>
+        Canceled = 4,
+    }
+}
