c38d3d6ebc85d6981624588e407f0071837092f5
PavelAndaliukevich <apv91@yandex.by>
2021-01-10 20:12:09 +0300

    добавлен код

--- src/Homework2/Homework2/Program.cs
+++ src/Homework2/Homework2/Program.cs
@@ -1,23 +1,111 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Homework2
+namespace DayInputHandler
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите дату для определения дня недели:");
-            string selection = Console.ReadLine();
-            bool dayweek = DateTime.TryParse(selection, out DateTime date);
-            if (dayweek)
+
+            DayInputHandler();
+
+            Console.WriteLine("Нажми любую кнопку для завершения программы.");
+            Console.ReadKey();
+
+        }
+
+        static void DayInputHandler()
+        {
+            DateTime DayToStart;
+            DateTime DayToEnd;
+
+            Console.Write("Введи начальную дату : ");
+            DayToStart = DayInputCheck(Console.ReadLine(), "Введи начальную дату : ");
+
+            Console.Write("Введи конечную дату : ");
+            DayToEnd = DayInputCheck(Console.ReadLine(), "Введи конечную дату : ");
+
+            if (DayToStart < DayToEnd)
             {
-                Console.WriteLine(date.DayOfWeek);
+                DayOutputHandler(DayToStart, DayToEnd);
             }
+
             else
             {
-                Console.WriteLine("Неправильная дата! Введите дату в формате dd mm yyyy");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка : Конечная дата больше начальной!");
+                Console.ResetColor();
+
+                DayInputHandler();
             }
-            Console.ReadLine();
+
+        }
+        static void DayOutputHandler(DateTime dayToStart, DateTime dayToEnd)
+        {
+            DayOfWeek dayOfWeek;
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Начальная дата : " + dayToStart.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+            Console.WriteLine("Конечная дата : " + dayToEnd.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+            Console.WriteLine("--------------------------------");
+
+            Console.Write("Введи день недели для поиска : ");
+
+            dayOfWeek = DayOfWeekInputCheck(Console.ReadLine(), "Введи день недели для поиска: ");
+
+            Console.WriteLine("День недели введен : " + dayOfWeek);
+            Console.WriteLine("--------------------------------");
+
+            while (dayToEnd >= dayToStart)
+            {
+                if (dayToStart.DayOfWeek == dayOfWeek)
+                {
+                    Console.WriteLine(dayToStart.ToString("dd/MM/yyyy - dddd", CultureInfo.InvariantCulture));
+                }
+                dayToStart = dayToStart.AddDays(1);
+            }
+
+        }
+
+        static DayOfWeek DayOfWeekInputCheck(string dayOfWeekInput, string request)
+        {
+            try
+            {
+                DayOfWeek dayOfWeek = Enum.Parse<DayOfWeek>(dayOfWeekInput);
+                return dayOfWeek;
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error : Invalid Input!");
+                Console.ResetColor();
+
+                Console.Write(request);
+
+                return DayOfWeekInputCheck(Console.ReadLine(), request);
+            }
+        }
+
+        static DateTime DayInputCheck(string dayInput, string request)
+        {
+            DateTime dayOutput;
+
+            try
+            {
+                return dayOutput = DateTime.Parse(dayInput);
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error : Input is Incorrect!");
+                Console.ResetColor();
+
+                Console.Write(request);
+
+                return DayInputCheck(Console.ReadLine(), request);
+            }
+
         }
     }
 }
