d0a039c3c2e8d782b8b6285736b783d6c9ca1a4e
PavelAndaliukevich <apv91@yandex.by>
2021-01-10 17:00:53 +0300

    init: add branch to dolist

---
+++ src/test/test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hw_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ReadKey();
+        }
+    }
+}
